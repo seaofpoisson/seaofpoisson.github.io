0ddef4571cb1aa00bec946a4c9478c6f9f75efa1
Ricardo Buring <ricardo.buring@gmail.com>
2025-06-17 17:06:13 +0200

    Add graph cohomology classes

---
+++ data/graph_cohomology/3_wheel_graph_cohomology_class/3_wheel_graph_cohomology_class.sage
@@ -0,0 +1,2 @@
+GC = UndirectedGraphComplex(QQ, implementation='vector', sparse=True)
+gamma_3 = GC.cohomology_basis(4,6)[0]
--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn main() -> std::io::Result<()> {
     }
     fs::create_dir("_site")?;
     fs::create_dir("_site/poisson_structures/")?;
+    fs::create_dir("_site/graph_cohomology/")?;
 
     // Copy static files.
     copy_dir_all("static", "_site/static")?;
@@ -34,13 +35,15 @@ fn main() -> std::io::Result<()> {
     env.add_template("index.html", &index).unwrap();
     let poisson_structure = fs::read_to_string("templates/poisson_structure.html")?;
     env.add_template("poisson_structure.html", &poisson_structure).unwrap();
-    let poisson_template = env.get_template("poisson_structure.html").unwrap();
+    let graph_cohomology_class = fs::read_to_string("templates/graph_cohomology_class.html")?;
+    env.add_template("graph_cohomology_class.html", &graph_cohomology_class).unwrap();
 
     // Create index.
     let index_template = env.get_template("index.html").unwrap();
     fs::write("_site/index.html", index_template.render(()).unwrap())?;
 
     // Create Poisson structures.
+    let poisson_template = env.get_template("poisson_structure.html").unwrap();
     for entry in fs::read_dir("data/poisson_structures")? {
         let entry = entry?;
         if entry.file_type()?.is_dir() {
@@ -58,6 +61,27 @@ fn main() -> std::io::Result<()> {
                 definition_code_gcaops => fs::read_to_string("data/poisson_structures/".to_owned() + &name + "/" + &name + ".sage")?,
             };
             fs::write("_site/poisson_structures/".to_owned() + &name + ".html", poisson_template.render(ctx).unwrap())?;
+        }
+    }
+
+    // Create graph cohomology classes.
+    let graph_cohomology_class_template = env.get_template("graph_cohomology_class.html").unwrap();
+    for entry in fs::read_dir("data/graph_cohomology/")? {
+        let entry = entry?;
+        if entry.file_type()?.is_dir() {
+            let name = entry.file_name().to_str().unwrap().to_owned();
+            let toml_path = name.clone() + "/" + &name + ".toml";
+            let toml_str = fs::read_to_string("data/graph_cohomology/".to_owned() + &toml_path)?;
+            let toml_table = toml_str.parse::<Table>().unwrap();
+            let ctx = context!{
+                name => toml_table["name"],
+                name_plain => toml_table["name_plain"],
+                alternative_names => toml_table["alternative_names"],
+                tags => toml_table["tags"],
+                references => toml_table["references"],
+                definition_code_gcaops => fs::read_to_string("data/graph_cohomology/".to_owned() + &name + "/" + &name + ".sage")?,
+            };
+            fs::write("_site/graph_cohomology/".to_owned() + &name + ".html", graph_cohomology_class_template.render(ctx).unwrap())?;
         }
     }
 
