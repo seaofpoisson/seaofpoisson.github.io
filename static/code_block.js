7c029330698a778b271807c5fd821a5f1c8e9df4
Ricardo Buring <ricardo.buring@gmail.com>
2025-07-07 10:30:11 +0200

    Make code block toggle persistent

--- static/code_block.js
+++ static/code_block.js
@@ -1,6 +1,17 @@
 function toggle_code_block(block_id) {
     let block = document.getElementById(block_id);
-    if (block.style.display == 'none') {
+    if (localStorage.getItem('show_code_blocks') === 'true') {
+        localStorage.setItem('show_code_blocks', 'false');
+        block.style.display = 'none';
+    } else {
+        localStorage.setItem('show_code_blocks', 'true');
+        block.style.display = 'table-row';
+    }
+}
+
+function show_or_hide_code_block(block_id) {
+    let block = document.getElementById(block_id);
+    if (localStorage.getItem('show_code_blocks') === 'true') {
         block.style.display = 'table-row';
     } else {
         block.style.display = 'none';
